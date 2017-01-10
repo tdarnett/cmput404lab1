f84cb1a8ee3330dce2a990b45a6538c7bc21645e
Taylor Arnett <tarnett@cs.ualberta.ca>
2017-01-10 09:09:58 -0700

    script prints script source code from repo

--- check_version.py
+++ check_version.py
@@ -4,6 +4,6 @@ import requests
 
 print requests.__version__
 
-response = requests.get("http://www.google.com")
+response = requests.get("https://raw.githubusercontent.com/tdarnett/cmput404lab1/master/check_version.py")
 print response.status_code
 print response.text
